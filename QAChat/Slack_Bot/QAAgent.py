8aba781f1c0dab9aaee415bf4852e26af1a2a175
Emanuel Erben <eerb@num42.de>
2023-05-12 14:55:41 +0200

    Added working Slackbot that gets direct messages and sends it to the framework
    Co-authored-by: Felix-012 <f.nuetzel@gmx.de>
    Co-authored-by: Emanuel Erben <emanuel.erben@gmail.com>
    Signed-off-by: Emanuel Erben <eerb@num42.de>

--- QAChat/Slack_Bot/QAAgent.py
+++ QAChat/Slack_Bot/QAAgent.py
@@ -1,15 +1,69 @@
+from queue import Queue
+from threading import Thread
 from BaseAgent import BaseAgent
-import os
+from slack_sdk import WebClient
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_bolt import App
+import re
+
+
+SLACK_TOKEN = "xoxb-1158014555255-5233887000391-2DdGhHHV2ZiM7xAaoP6gLXjc"
+SLACK_APP_TOKEN = "xapp-1-A057NUGLKSM-5242120534438-721d74aa75c425b370fcac569e4f48cec8f9010b77904cb5d2efceaaa2e2e0c9"
+SIGNING_SECRET = "0af7f1d9fc5406de3ebefff175f4b852"
 
 
 class QAAgent(BaseAgent):
 
     def __init__(self):
         super().__init__()
+        self.app = App(token=SLACK_TOKEN)
+        self.client = WebClient(token=SLACK_TOKEN)
+        self.handler = SocketModeHandler(self.app, SLACK_APP_TOKEN)
 
-    def receive_question(self, question):
-        self.api_interface.listen_for_requests(question)
+        # Create a dictionary to hold say functions for each user
+        self.say_functions = {}
 
-    def receive_answer(self, answer):
-        # pass answer to slackbotapi
-        pass
+        # Create a queue to hold responses
+        self.response_queue = Queue()
+
+        # Create a worker thread to send responses
+        self.response_worker = Thread(target=self.send_responses)
+        self.response_worker.start()
+
+    def send_responses(self):
+        while True:
+            # Get a response from the queue and send it
+            response = self.response_queue.get()
+            user_id, answer = response
+            say = self.say_functions.get(user_id)
+            if say is not None:
+                say(answer)
+
+    def receive_question(self, question, user_id):
+        self.api_interface.listen_for_requests(question, self, user_id)
+
+    def receive_answer(self, answer, user_id):
+        # Put the answer and user_id in the queue instead of sending it directly
+        self.response_queue.put((user_id, answer))
+
+    def process_question(self, body, say):
+        text = body['event']['text']
+        user_id = body['event']['user']
+        say(text)
+        print(text)
+
+        # Store the say function for this user
+        self.say_functions[user_id] = say
+
+        # Use a separate thread to call receive_question
+        thread = Thread(target=self.receive_question, args=(text, user_id))
+        thread.start()
+
+    def start(self):
+        self.handler.app.message(re.compile('.*'))(self.process_question)
+        self.handler.start()
+
+
+if __name__ == '__main__':
+    agent = QAAgent()
+    agent.start()
