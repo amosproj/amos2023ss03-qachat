2ac647f254223f54c8a2cdf62f0af9acd732207c
Emanuel Erben <eerb@num42.de>
2023-05-12 14:54:16 +0200

    APIInterface to support userid handling
    Co-authored-by: Felix-012 <f.nuetzel@gmx.de>
    Co-authored-by: Emanuel Erben <emanuel.erben@gmail.com>
    Signed-off-by: Emanuel Erben <eerb@num42.de>

--- QAChat/QA_Bot/APIInterface.py
+++ QAChat/QA_Bot/APIInterface.py
@@ -1,20 +1,18 @@
 from QAChat.QA_Bot.QABot import QABot
 import QAChat.Slack_Bot.BaseAgent as BaseAgent
 
+
 class APIInterface:
 
     def __init__(self, qa_bot=None):
         self.qa_bot = qa_bot
-        # TODO: This needs remodeling to handle multiple Request at once maybe ???
         self.sender: BaseAgent = None
 
-    # forwards the question sent by the slackbot to the qabot
-    def listen_for_requests(self, question, sender):
+    def listen_for_requests(self, question, sender, user_id):
         self.sender = sender
         if self.qa_bot is None:
             self.qa_bot = QABot(self)
-        self.qa_bot.answer_question(question)
+        self.qa_bot.answer_question(question, user_id)
 
-    # forwards the answer sent by the qabot to the userAgent
-    def forward_answer(self, answer):
-        self.sender.receive_answer(answer)
+    def forward_answer(self, answer, user_id):
+        self.sender.receive_answer(answer, user_id)
