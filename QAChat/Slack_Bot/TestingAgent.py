f66514bd2910aa6a6fc3265c9ec42664115a953e
Emanuel Erben <eerb@num42.de>
2023-05-12 14:53:24 +0200

    Testing Agent reworked to old functionality
    Co-authored-by: Felix-012 <f.nuetzel@gmx.de>
    Co-authored-by: Emanuel Erben <emanuel.erben@gmail.com>
    Signed-off-by: Emanuel Erben <eerb@num42.de>

--- QAChat/Slack_Bot/TestingAgent.py
+++ QAChat/Slack_Bot/TestingAgent.py
@@ -1,21 +1,10 @@
 import tkinter as tk
-from slack_bolt import App
 from BaseAgent import BaseAgent
-import slack
-from slack_bolt.adapter.socket_mode import SocketModeHandler
-import re
 
 '''
     This is just an easy Testing Bot for testing reasons and should not be part of the final release. 
     Its purpose is to send and receive answers from our system, without having a functioning SlackBot or other.
 '''
-
-SLACK_TOKEN = "INSERT"
-SLACK_APP_TOKEN = "INSERT"
-app = App(token=SLACK_TOKEN)
-SIGNING_SECRET = "INSERT"
-client = slack.WebClient(token=SLACK_TOKEN)
-client.chat_postMessage(channel='#bot-test', text='Hi')
 
 
 class TestingAgent(BaseAgent):
@@ -36,29 +25,15 @@ class TestingAgent(BaseAgent):
         self.result_label = tk.Label(master, text="")
         self.result_label.pack()
 
-    def receive_question(self, question=None):
+    def receive_question(self, question=None, user_id=None):
         input_text = self.entry.get()
         self.api_interface.listen_for_requests(input_text, self)
 
-    def receive_answer(self, answer):
+    def receive_answer(self, answer, user_id=None):
         self.result_label.config(text=answer)
 
 
-@app.message(re.compile('.*'))
-def process_question(body, say):
-    text = body['event']['text']
-    say("I cannot answer if " + text)
-
-
-@app.event("app_mention")
-def mention_handler(body, say):
-    user = body['user']
-    say(f"Hi there, <@{user}>!")
-
-
 if __name__ == '__main__':
-    handler = SocketModeHandler(app, SLACK_APP_TOKEN)
-    handler.start()
-    # root = tk.Tk()
-    # gui = TestingAgent(root)
-    # root.mainloop()
+    root = tk.Tk()
+    gui = TestingAgent(root)
+    root.mainloop()
