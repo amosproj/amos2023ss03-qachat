2d1d184622fdb40e9cb2f85475212aa2230b5634
Jesse Palarus <jtsharkjtshark@gmail.com>
2023-05-13 17:34:09 +0200

    Add code framework for data preprocessing (issue #37) Co-authored-by: Jesse Palarus <jtsharkjtshark@gmail.com> Co-authored-by: Amela Pucic <amela1999@hotmail.de>

---
+++ QAChat/Data_Processing/data_preprocessor.py
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2023 Jesse Tim Palarus
+# SPDX-FileCopyrightText: 2023 Amela Pucic
+
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import List
+
+from document_embedder import DataInformation
+
+
+class DataPreprocessor(ABC):
+    @abstractmethod
+    def load_preprocessed_data(self, before: datetime, after: datetime) -> List[DataInformation]:
+        """
+        Loads preprocessed data of a specific type that was created or modified
+        within a certain timeframe.
+
+        The specific data type is defined by the superclass of the instance calling this method.
+
+        Parameters:
+        before (type): The end of the timeframe during which the data was created or modified.
+        after (type): The start of the timeframe during which the data was created or modified.
+
+        Returns:
+        List[DataInformation]: A list of DataInformation instances representing the preprocessed data.
+        """
+        pass
---
+++ QAChat/Data_Processing/document_embedder.py
@@ -0,0 +1,70 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2023 Jesse Tim Palarus
+# SPDX-FileCopyrightText: 2023 Amela Pucic
+
+import os
+from datetime import datetime
+from enum import Enum
+
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import SupabaseVectorStore
+from supabase.client import create_client
+from dateutil import parser
+
+
+class DataSource(Enum):
+    SLACK = "slack"
+    CONFLUENCE = "confluence"
+    DRIVE = "drive"
+    DUMMY = "dummy"
+
+
+class DataInformation:
+    def __init__(self, id: str, last_changed: datetime, typ: DataSource, text: str):
+        self.id = id
+        self.last_changed = last_changed
+        self.typ = typ
+        self.text = text
+
+
+class DocumentEmbedder:
+
+    def __init__(self):
+        self.embedder = HuggingFaceInstructEmbeddings(
+            model_name="hkunlp/instructor-xl",
+        )
+
+        supabase_url = os.environ.get("SUPABASE_URL")
+        supabase_key = os.environ.get("SUPABASE_SERVICE_KEY")
+        self.supabase = create_client(supabase_url, supabase_key)
+        self.vector_store = SupabaseVectorStore(self.supabase, embedding=self.embedder, table_name="data_embedding",
+                                                query_name="match_data")
+
+    def store_information_in_database(self, typ: DataSource):
+        if typ == DataSource.DUMMY:
+            from dummy_preprocessor import DummyPreprocessor
+            data_preprocessor = DummyPreprocessor()
+        else:
+            raise ValueError("Invalid data source type")
+
+        last_updated_database_result = self.supabase.table("last_update_per_data_typ").select("last_update").eq("type",
+                                                                                                                typ.value).execute()
+        if len(last_updated_database_result.data) == 0:
+            last_updated = datetime(1970, 1, 1)
+        else:
+            last_updated = parser.parse(last_updated_database_result.data[0]["last_update"]).replace(tzinfo=None)
+
+        current_time = datetime.now()
+        all_changed_data = data_preprocessor.load_preprocessed_data(current_time, last_updated)
+
+        if len(all_changed_data) != 0:
+            ids = {data.id for data in all_changed_data}
+            self.supabase.rpc("delete_old_embeddings", {"ids": list(ids), "delete_type": typ.value}).execute()
+
+            self.vector_store.add_texts(
+                [data.text for data in all_changed_data],
+                [{"last_changed": data.last_changed.isoformat(), "type": typ.value, "id": data.id} for data in
+                 all_changed_data], )
+
+        self.supabase.table("last_update_per_data_typ").upsert(
+            {"type": typ.value, "last_update": current_time.isoformat()}, ).execute()
