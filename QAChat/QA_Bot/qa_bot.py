f336cd8316188ca1ec7e46849ca7bebb189cb6e6
Emanuel Erben <emanuel.erben@gmail.com>
2023-05-15 14:50:54 +0200

    Added translation into current demoflow
    Co-authored-by: Emanuel Erben <emanuel.erben@gmail.com>
    Co-authored-by: Felix-012 <f.nuetzel@gmx.de>
    Signed-off-by: Emanuel Erben <emanuel.erben@gmail.com>

--- QAChat/QA_Bot/qa_bot.py
+++ QAChat/QA_Bot/qa_bot.py
@@ -6,6 +6,8 @@ import QAChat.QA_Bot.api_interface as APIInterface
 import json
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 
+from QAChat.Data_Processing.deepL_translator import DeepL_Translator
+
 
 class QABot:
 
@@ -18,7 +20,7 @@ class QABot:
         self.i = 0
 
     def ask_question(self, question, context):
-        return self.answer+context
+        return question
 
     def get_embedding(self, question):
         return self.embeddings.embed_query(question)
@@ -27,14 +29,18 @@ class QABot:
         question_embedding_json = json.dumps(question_embedding)
         return "Test"
 
+    def translate_question(self, question):
+        return DeepL_Translator().receive_question(question)
+
     def answer_question(self, question, user_id):
 
         self.answer = ""
         if self.api_interface is None:
             self.api_interface = APIInterface
-        question_embedding = self.get_embedding(question)
+        translated_question = self.translate_question(question)
+        question_embedding = self.get_embedding(translated_question)
         self.context = self.sim_search(question_embedding)
-        self.answer = self.ask_question(question, self.context)
+        self.answer = self.ask_question(translated_question, self.context)
 
         self.answer += ": Resolved Questions:" + str(self.i+1)
         self.i += 1
