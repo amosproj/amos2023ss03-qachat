e8f902aceb34c64fdc54641bd4d8386e19b4c337
Felix-012 <f.nuetzel@gmx.de>
2023-05-16 11:47:35 +0200

    finished and cleaned up translation_tests.py

--- QAChat/Translation_Testing/translation_tests.py
+++ QAChat/Translation_Testing/translation_tests.py
@@ -1,10 +1,22 @@
+import csv
 from datetime import datetime
 
 from nltk import word_tokenize
 from nltk.translate.bleu_score import sentence_bleu
 from rapidfuzz.distance import Levenshtein
-from QAChat.Data_Processing.deepL_translator import DeepLTranslator, load_data_from_csv
+from QAChat.Data_Processing.deepL_translator import DeepLTranslator
 import numpy as np
+import datetime
+
+
+def load_data_from_csv(file_path):
+    data = []
+    with open(file_path, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            for dataset in row:
+                data.append(dataset)
+    return data
 
 
 def test_translation():
@@ -16,25 +28,25 @@ def test_translation():
     total_start = datetime.now()
     comp_arr = []
     bleu_score_arr = []
-    for i in data:
-        start_text = i
+    for phrase in data:
+        start_text = phrase
         comp_arr.append(start_text)
         start = datetime.now()
-        translation = translator.translate_german_english(i)
+        translation = translator.translate_german_english(phrase)
         comp_arr.append(translation)
         result = translator.translate_english_german(translation)
         comp_arr.append(result)
         end = datetime.now()
         diff = end - start
         diff_arr.append(diff)
-        original = word_tokenize(i.lower())
+        original = word_tokenize(phrase.lower())
         translated = word_tokenize(result.lower())
         bleu_score = sentence_bleu([original], translated)
         bleu_score_arr.append(bleu_score)
         # print(diff.seconds)
         # compare two strings and give score of how similar they are
-        distance = Levenshtein.distance(i, result)
-        max_length = max(len(i), len(result))
+        distance = Levenshtein.distance(phrase, result)
+        max_length = max(len(phrase), len(result))
         similarity_score.append(1 - (distance / max_length))
 
     total_end = datetime.now()
@@ -69,6 +81,28 @@ def compute_metrics_bleu():
     median = np.median(blue_scores)
     variance = np.var(blue_scores)
 
+    print("BLEU:")
+    print(f"Maximum: {max_value}")
+    print(f"Minimum: {min_value}")
+    print(f"Average: {average}")
+    print(f"Median: {median}")
+    print(f"Variance: {variance}")
+
+def compute_metrics_levenshtein():
+    # Open the file and read the lines
+    with open('similiarity_score.txt', 'r') as f:
+        lines = f.readlines()
+
+    # Convert each line to a float and store in a list
+    sim_scores = [float(line.strip()) for line in lines]
+
+    max_value = np.max(sim_scores)
+    min_value = np.min(sim_scores)
+    average = np.mean(sim_scores)
+    median = np.median(sim_scores)
+    variance = np.var(sim_scores)
+
+    print("Levenshtein:")
     print(f"Maximum: {max_value}")
     print(f"Minimum: {min_value}")
     print(f"Average: {average}")
@@ -81,21 +115,26 @@ def compute_metrics_time():
     with open('time_diff.txt', 'r') as f:
         lines = f.readlines()
 
-    # Convert each line to a float and store in a list
-    blue_scores = [float(line.strip()) for line in lines]
+    time_scores = []
 
-    max_value = np.max(blue_scores)
-    min_value = np.min(blue_scores)
-    average = np.mean(blue_scores)
-    median = np.median(blue_scores)
-    variance = np.var(blue_scores)
+    for line in lines:
+        line = line.strip()
+        hours, minutes, seconds = map(float, line.split(':'))
+        time_scores.append(datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds).total_seconds())
 
+    max_value = np.max(time_scores)
+    min_value = np.min(time_scores)
+    average = np.mean(time_scores)
+    median = np.median(time_scores)
+    print("Time:")
     print(f"Maximum: {max_value}")
     print(f"Minimum: {min_value}")
     print(f"Average: {average}")
     print(f"Median: {median}")
-    print(f"Variance: {variance}")
 
 
 if __name__ == '__main__':
-    test_translation()
+    # test_translation()
+    compute_metrics_bleu()
+    compute_metrics_levenshtein()
+    compute_metrics_time()
