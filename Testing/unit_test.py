b3d486fe6a874b10501e7aafb1e9a2f6678a6a4d
Felix-012 <f.nuetzel@gmx.de>
2023-05-20 11:55:20 +0200

    black autoformat testing

--- Testing/unit_test.py
+++ Testing/unit_test.py
@@ -1,10 +1,12 @@
 import unittest
 from add_file import add
 
+
 class TestAdd(unittest.TestCase):
     def test_add(self):
-        result = add(1,1)
+        result = add(1, 1)
         self.assertEqual(result, 2)
+
 
 if __name__ == "__main__":
     unittest.main()
