2463cf6fd8a6d8b38bc174d949832ecb5d65189b
Felix-012 <f.nuetzel@gmx.de>
2023-05-20 15:04:21 +0200

    reworked the unit test to work without secrets

--- Testing/unit_test.py
+++ Testing/unit_test.py
@@ -1,21 +1,24 @@
 import re
-from unittest.mock import MagicMock
-
+from unittest.mock import patch, MagicMock
 from QAChat.Slack_Bot.qa_agent import QAAgent
-
-from unittest.mock import patch
 
 
 def test_run_agent():
-    agent = QAAgent()
-    # Mock the methods that get called in the start method
-    agent.handler.app.message = MagicMock()
-    agent.handler.start = MagicMock()
+    with patch('QAChat.QA_Bot.qa_bot.QABot.__init__', return_value=None), \
+            patch('slack_bolt.App.__init__', return_value=None), \
+            patch('slack_bolt.adapter.socket_mode.SocketModeHandler.__init__', return_value=None):
+        agent = QAAgent()
 
-    # Run the method
-    agent.start()
-    with patch.object(agent.handler, 'start'):
+        # Mock the app and attach it to the handler
+        agent.handler.app = MagicMock()
+        agent.handler.app.message = MagicMock()
+        agent.handler.start = MagicMock()
+
+        # Run the method
         agent.start()
-    # Check if the methods were called
-    agent.handler.app.message.assert_called_with(re.compile('.*'))
-    agent.handler.start.assert_called()
+        with patch.object(agent.handler, 'start'):
+            agent.start()
+
+        # Check if the methods were called
+        agent.handler.app.message.assert_called_with(re.compile('.*'))
+        agent.handler.start.assert_called()
