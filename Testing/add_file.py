0ebc1717e7dd9aa3436d23b497920e9915440a44
Felix-012 <f.nuetzel@gmx.de>
2023-05-20 13:49:39 +0200

    adds specified working directory to workflow

--- Testing/add_file.py
+++
@@ -1,8 +0,0 @@
-
-def add(a, b):
-    return a+ b
-
-
-def add2(a, b):
-    l = ["aaknsdksndkasndasdasdaisgdbiuas", "sajkdbhaisdhioashdiuasgdikashdjhasvdjasndkjhasvbdiuapsidhiuasbd", "jsavdhjvasudvuasbdioabshdvaishdiuyasvdoiasbdivasudib", "jbasiudhausdbiuasbhdoasbduahsdjabsdklansbdjkhabskdnkajsvdjbasdkjbakjsbdasd", "klsajvdhasvbdihabshjdvbkalsndbhjavsbdbashjdvbkjasbdhjasdkjvasvdhajhsdvkjasbdhjabskdbaskjd", "jkvbashjdbasdvaksjdbjhasvdbkasbdvasbdasdvbasbdvabsndbasvhdbasndbsadnk"]
-    return a+ b
