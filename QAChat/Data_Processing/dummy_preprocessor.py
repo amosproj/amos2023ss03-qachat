4b19439995de93b8c51c17795fe9fede4694b473
Jesse <jtsharkjtshark@gmail.com>
2023-05-20 18:47:06 +0200

    Add more dummy data to test things better (issue 78)
    Co-authored-by: Jesse Palarus <jtsharkjtshark@gmail.com>
    Co-authored-by: Hafidz Arifin <hafidz.harifin@gmail.com>

--- QAChat/Data_Processing/dummy_preprocessor.py
+++ QAChat/Data_Processing/dummy_preprocessor.py
@@ -5,25 +5,17 @@
 from datetime import datetime
 from typing import List
 
+import pandas as pd
 from document_embedder import DataInformation, DataSource
 from data_preprocessor import DataPreprocessor
 
 
 class DummyPreprocessor(DataPreprocessor):
     def load_preprocessed_data(self, before: datetime, after: datetime) -> List[DataInformation]:
-        raw_data = [
-            DataInformation(id="1", last_changed=datetime(2021, 1, 1), typ=DataSource.DUMMY,
-                            text="This is a dummy text."),
-            DataInformation(id="2", last_changed=datetime(2021, 1, 2), typ=DataSource.DUMMY,
-                            text="This is another dummy text."),
-            DataInformation(id="3", last_changed=datetime(2021, 1, 3), typ=DataSource.DUMMY,
-                            text="This is a third dummy text."),
-            DataInformation(id="4", last_changed=datetime(2023, 1, 3), typ=DataSource.DUMMY,
-                            text="This is another dummy text, that is changed."),
-            DataInformation(id="5", last_changed=datetime(2023, 1, 3), typ=DataSource.DUMMY,
-                            text="To connect to the QAWare SSH server please use the following command: ssh -p 2222 qaware@localhost"),
-            DataInformation(id="6", last_changed=datetime(2023, 1, 3), typ=DataSource.DUMMY,
-                            text="You can find our office in: Aschauer Straße 32. The office is located in the 3rd floor. Also there is a cantine in the 2nd floor for employees."),
-        ]
+        df = pd.read_csv("../../DummyData/qa.csv", sep=";")
+        raw_data = []
+        for index, row in df.iterrows():
+            raw_data.append(DataInformation(id=f"{index}", last_changed=datetime(2021, 1, 1), typ=DataSource.DUMMY,
+                                            text=row["Answer"]))
 
         return [data for data in raw_data if after < data.last_changed <= before]
