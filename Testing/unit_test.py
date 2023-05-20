48f1172d6c2484d44e375ae1becd7d92140e68f1
Felix-012 <f.nuetzel@gmx.de>
2023-05-20 13:33:25 +0200

    mocks socket handler and web client in test_run_agent

--- Testing/unit_test.py
+++ Testing/unit_test.py
@@ -4,19 +4,25 @@ from unittest.mock import MagicMock
 
 from QAChat.Slack_Bot.qa_agent import QAAgent
 
+from unittest.mock import patch
 
-def test_run_agent():
+
+
+@patch('slack_sdk.WebClient')
+@patch('slack_bolt.adapter.socket_mode.SocketModeHandler')
+def test_run_agent(mocked_socket_mode_handler, mocked_web_client):
     agent = QAAgent()
     # Mock the methods that get called in the start method
     agent.handler.app.message = MagicMock()
     agent.handler.start = MagicMock()
 
-    # Run the method
+        # Run the method
     agent.start()
-
+    with patch.object(agent.handler, 'start'):
+        agent.start()
     # Check if the methods were called
-    agent.handler.app.message.assert_called_once_with(re.compile('.*'))
-    agent.handler.start.assert_called_once()
+    agent.handler.app.message.assert_called_with(re.compile('.*'))
+    agent.handler.start.assert_called()
 
 
 class UnitTest:
