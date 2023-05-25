f5e43f4e63c9dad4dd1411964e4386d683c60d67
Hafidz Arifin <hafidz.harifin@gmail.com>
2023-05-26 01:02:19 +0200

    Modified code to load confluence data to supabase instead dummy data
    Signed-off-by: Hafidz Arifin <hafidz.harifin@gmail.com>

--- QAChat/Data_Processing/main.py
+++ QAChat/Data_Processing/main.py
@@ -1,8 +1,15 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2023 Jesse Tim Palarus
 # SPDX-FileCopyrightText: 2023 Amela Pucic
+# SPDX-FileCopyrightText: 2023 Hafidz Arifin
 
 from document_embedder import DocumentEmbedder, DataSource
+from dotenv import load_dotenv
+import os
+
+load_dotenv("../tokens.env")
+CONFLUENCE_TOKEN = os.getenv("CONFLUENCE_TOKEN")
 
 if __name__ == '__main__':
-    DocumentEmbedder().store_information_in_database(DataSource.DUMMY)
+
+    DocumentEmbedder().store_information_in_database(DataSource.CONFLUENCE)
