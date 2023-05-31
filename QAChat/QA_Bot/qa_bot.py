5db55787f6ba9f2d132b7c2b801d22e67f231fc0
Emanuel Erben <emanuel.erben@gmail.com>
2023-05-31 22:45:11 +0200

    Fix error when getting answer
    Co-authored-by: Emanuel Erben <emanuel.erben@gmail.com>
    Co-authored-by: Felix-012 <f.nuetzel@gmx.de>

--- QAChat/QA_Bot/qa_bot.py
+++ QAChat/QA_Bot/qa_bot.py
@@ -135,5 +135,5 @@ class QABot:
         translation = self.translate_text(question)
         context = self.__sim_search(translation.text)
         answer = self.__answer_question_with_context(question, context)
-        answer = self.translate_text(answer, translation.detected_source_lang)
+        answer = self.translate_text(answer, translation.detected_source_lang).text
         return answer
