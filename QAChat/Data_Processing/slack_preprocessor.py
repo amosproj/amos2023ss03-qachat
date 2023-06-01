7a3fd1bb63cc3e052db6a07e00bb876708c8289a
Emanuel Erben <emanuel.erben@gmail.com>
2023-06-01 15:26:20 +0200

    create new class
    Co-authored-by: Emanuel Erben <emanuel.erben@gmail.com>
    Co-authored-by: Felix-012 <f.nuetzel@gmx.de>
    Co-authored-by: Hafidz Arifin <hafidz.harifin@gmail.com>
    Signed-off-by: Emanuel Erben <emanuel.erben@gmail.com>

---
+++ QAChat/Data_Processing/slack_preprocessor.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2023 Emanuel Erben
+# SPDX-FileCopyrightText: 2023 Felix Nützel
+# SPDX-FileCopyrightText: 2023 Hafidz Arifin
+class SlackPreprocessor(DataPreprocessor):
+
+    def __init__(self):
+if __name__ == '__main__':
+    preprocess = SlackPreprocessor()
