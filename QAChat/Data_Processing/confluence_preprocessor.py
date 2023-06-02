1617ac8fd9a1c3bf21d3cc7853964df61b9ee4a9
Hafidz Arifin <hafidz.harifin@gmail.com>
2023-06-02 13:30:54 +0200

    restructure Co-authored-by: Hafidz Arifin <hafidz.harifin@gmail.com> Co-authored-by: ABDELKADER ALKADOUR <BASICKADOUR@GMAIL.COM>
    Signed-off-by: Hafidz Arifin <hafidz.harifin@gmail.com>

--- QAChat/Data_Processing/confluence_preprocessor.py
+++ QAChat/Data_Processing/confluence_preprocessor.py
@@ -10,109 +10,117 @@ from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from datetime import datetime
 from typing import List
-import logging
 import os
 
 load_dotenv("../tokens.env")
 
+# Get Confluence API credentials from environment variables
+CONFLUENCE_ADDRESS = os.getenv("CONFLUENCE_ADDRESS")
+CONFLUENCE_USERNAME = os.getenv("CONFLUENCE_USERNAME")
 CONFLUENCE_TOKEN = os.getenv("CONFLUENCE_TOKEN")
 
 
 class ConfluencePreprocessor(DataPreprocessor):
-    def load_preprocessed_data(self, before: datetime, after: datetime) -> List[DataInformation]:
 
-        all_spaces = []
-        all_pages_id = []
-        all_page_information = []
+    def __init__(self):
+        self.confluence = Confluence(
+            url=CONFLUENCE_ADDRESS,
+            username=CONFLUENCE_USERNAME,
+            password=CONFLUENCE_TOKEN,
+            cloud=True)
+        self.all_spaces = []
+        self.all_pages_id = []
+        self.all_page_information = []
 
-        try:
+    def get_all_spaces(self):
+        start = 0
+        limit = 500
+
+        # Get all spaces first
+        while True:
+
+            # url:      to the confluence parameter
+            # username: to your email used in confluence
+            # password: if confluence is cloud set Confluence API Token https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/
+            #           if confluence is server set your password
+            # cloud:    True if confluence is cloud version
+
+            # Get all confluence spaces from the confluence instance
+            spaces_data = self.confluence.get_all_spaces(start=start, limit=limit, expand=None)
+
+            # exclude personal/user spaces only global spaces
+            for space in spaces_data['results']:
+                if space['type'] == 'global':
+                    self.all_spaces.append(space)
+
+            # Check if there are more spaces
+            if len(spaces_data) < limit:
+                break
+            start = start + limit
+
+    def get_all_page_ids_from_spaces(self):
+        # Get all pages from a space
+        for space in self.all_spaces:
 
             start = 0
-            limit = 500
+            limit = 100
 
-            # Get all spaces first
             while True:
+                pages_data = self.confluence.get_all_pages_from_space(space['key'], start=start, limit=limit,
+                                                                      status=None,
+                                                                      expand=None, content_type='page')
+                #  Get all page id
+                for page in pages_data:
+                    self.all_pages_id.append(page['id'])
 
-                # TODO: need to change parameter
-                # url:      to the confluence parameter
-                # username: to your email used in confluence
-                # password: if confluence is cloud set Confluence API Token https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/
-                #           if confluence is server set your password
-                # cloud:    True if confluence is cloud version
-
-                confluence = Confluence(
-                    url='https://qaware-confluence2.atlassian.net/',
-                    username='user@email.com',
-                    password=CONFLUENCE_TOKEN,
-                    cloud=True)
-
-                # Get all confluence spaces from the confluence instance
-                spaces_data = confluence.get_all_spaces(start=start, limit=limit, expand=None)
-
-                # exclude personal/user spaces only global spaces
-                for space in spaces_data['results']:
-                    if space['type'] == 'global':
-                        all_spaces.append(space)
-
-                # Check if there are more spaces
-                if len(spaces_data) < limit:
+                # Check if there are more pages
+                if len(pages_data) < limit:
                     break
                 start = start + limit
 
+    def get_relevant_data_from_pages(self):
+        # Get all relevant information from each page
+        for page_id in self.all_pages_id:
+            # Get page by id
+            page_with_body = self.confluence.get_page_by_id(page_id, expand='body.storage, version', status=None,
+                                                            version=None)
+            page_info = self.confluence.get_page_by_id(page_id, expand=None, status=None, version=None)
 
-            # Get all pages from a space
-            for space in all_spaces:
+            # Set final parameters for DataInformation
+            last_changed = self.get_last_modified_formated_date(page_info)
+            text = self.get_raw_text_from_page(page_with_body)
 
-                start = 0
-                limit = 100
+            # Add to list of DataInformation
+            self.all_page_information.append(
+                DataInformation(id=page_id, last_changed=last_changed, typ=DataSource.CONFLUENCE, text=text))
 
-                while True:
-                    pages_data = confluence.get_all_pages_from_space(space['key'], start=start, limit=limit, status=None,
-                                                                     expand=None, content_type='page')
-                    #  Get all page id
-                    for page in pages_data:
-                        all_pages_id.append(page['id'])
+    def get_last_modified_formated_date(self, page_info) -> datetime:
+        # Get date of last modified page
+        data_last_changed = page_info['version']['when']
+        year_string = data_last_changed[0:4]
+        month_string = data_last_changed[5:7]
+        day_string = data_last_changed[8:10]
 
-                    # Check if there are more pages
-                    if len(pages_data) < limit:
-                        break
-                    start = start + limit
+        # Convert string to int
+        year = int(year_string)
+        month = int(month_string)
+        day = int(day_string)
 
-            # Get all relevant information from each page
-            for page_id in all_pages_id:
-                # Get page by id
-                page_with_body = confluence.get_page_by_id(page_id, expand='body.storage, version', status=None,
-                                                           version=None)
-                page_info = confluence.get_page_by_id(page_id, expand=None, status=None, version=None)
+        return datetime(year, month, day)
 
-                # Get page content
-                page_in_html = page_with_body['body']['storage']['value']
+    def get_raw_text_from_page(self, page_with_body) -> str:
+        # Get page content
+        page_in_html = page_with_body['body']['storage']['value']
 
-                # Convert HTML page content to raw text
-                page_in_raw_text = BeautifulSoup(page_in_html, features="html.parser")
+        # Convert HTML page content to raw text
+        page_in_raw_text = BeautifulSoup(page_in_html, features="html.parser")
 
-                # Get date of last modified page
-                data_last_changed = page_info['version']['when']
-                year_string = data_last_changed[0:4]
-                month_string = data_last_changed[5:7]
-                day_string = data_last_changed[8:10]
+        return page_in_raw_text.get_text()
 
-                # Convert string to int
-                year = int(year_string)
-                month = int(month_string)
-                day = int(day_string)
+    def load_preprocessed_data(self, before: datetime, after: datetime) -> List[DataInformation]:
 
-                # Set final parameters for DataInformation
-                last_changed = datetime(year, month, day)
-                text = page_in_raw_text.get_text()
+        self.get_all_spaces()
+        self.get_all_page_ids_from_spaces()
+        self.get_relevant_data_from_pages()
 
-                # Add to list of DataInformation
-                all_page_information.append(
-                    DataInformation(id=page_id, last_changed=last_changed, typ=DataSource.CONFLUENCE, text=text))
-
-            return all_page_information
-
-
-
-        except Exception as e:
-            logging.error(e)
+        return [data for data in self.all_page_information if after < data.last_changed <= before]
