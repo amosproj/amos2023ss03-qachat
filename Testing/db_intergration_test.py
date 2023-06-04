a385e176d5cccb9d89e872e40af8542bc31469fa
abdelkader alkadour <basickadour@gmail.com>
2023-06-04 14:50:33 +0200

    update db_integration_test Co-authored-by: Abdelkader Alkadour <basickadour@gmail.conm> Co-authored-by: Amela Pucic <amela1999@hotmail.de>

--- Testing/db_intergration_test.py
+++ Testing/db_intergration_test.py
@@ -1,17 +1,44 @@
 import unittest
-from QAChat.db_interface import DBInterface
+from langchain.vectorstores import SupabaseVectorStore
 import os
+from supabase.client import create_client
+import supabase
+from dotenv import load_dotenv
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+
+
+
+load_dotenv("/Users/kad/Desktop/AMOS/amos2023ss03-qachat/tokens.env")
+
+class Embedder():
+    def __init__(self):
+        pass
 
 
 class DatabaseIntegrationTest(unittest.TestCase):
+    def setUp(self):
+        self.embedder = Embedder()
+    
+    def test_integration_scenario_positive(self):
+        supabase_client = create_client(os.environ.get("SUPABASE_URL"), os.environ.get("SUPABASE_SERVICE_KEY"))
+        db = SupabaseVectorStore(supabase_client, embedding=self.embedder, table_name="data_embedding")
+        self.assertEqual(db.table_name, "data_embedding")
+    
+    def test_integration_scenario_negative_url(self):
+        with self.assertRaises(supabase.client.SupabaseException):
+            supabase_client = create_client(None, os.environ.get("SUPABASE_SERVICE_KEY"))
+            db = SupabaseVectorStore(supabase_client, embedding=self.embedder, table_name="data_embedding")
 
-    def test_init(self):
-        try:
-            db = DBInterface()
-            print("SUPABASE_URL: ", os.environ['SUPABASE_URL'])
-        except ConnectionError as e:
-            print("Connection Error:", e)
-      
+    def test_integration_scenario_negative_url_format(self):
+        with self.assertRaises(supabase.client.SupabaseException):
+            supabase_client = create_client("htp://google.com", os.environ.get("SUPABASE_SERVICE_KEY"))
+            db = SupabaseVectorStore(supabase_client, embedding=self.embedder, table_name="data_embedding")
+    
+    def test_integration_scenario_negative_api_key(self):
+        with self.assertRaises(supabase.client.SupabaseException):
+            supabase_client = create_client(os.environ.get("SUPABASE_URL"), None)
+            db = SupabaseVectorStore(supabase_client, embedding=self.embedder, table_name="data_embedding")
+
 
 if __name__ == '__main__':
-    unittest.main()
+      unittest.main()
