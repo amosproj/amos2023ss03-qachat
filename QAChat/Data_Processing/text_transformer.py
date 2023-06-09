ec12d34dd467643e144edd9d7992839acf005ebf
Emanuel Erben <emanuel.erben@gmail.com>
2023-06-09 12:10:35 +0200

    some fixes
    Co-authored-by: Emanuel Erben <emanuel.erben@gmail.com>
    Co-authored-by: Felix-012 <f.nuetzel@gmx.de>

--- QAChat/Data_Processing/text_transformer.py
+++ QAChat/Data_Processing/text_transformer.py
@@ -1,7 +1,9 @@
 from QAChat.Data_Processing.deepL_translator import DeepLTranslator
 from langchain.text_splitter import NLTKTextSplitter
+import nltk
 
-CHUNK_SIZE = 100
+CHUNK_SIZE = 200
+CHUNK_OVERLAP = 50
 
 
 def transform_text_to_chunks(data_information_list):
@@ -10,16 +12,18 @@ def transform_text_to_chunks(data_information_list):
     for data_information in data_information_list:
         # translate text
         translator = DeepLTranslator()
-        data_information.text = translator.translate_to(data_information.text, "EN-US")
+        data_information.text = translator.translate_to(data_information.text, "EN-US").text
 
         # split the text
-        text_splitter = NLTKTextSplitter(chunk_size=CHUNK_SIZE)
+        nltk.download('punkt')
+        text_splitter = NLTKTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
+        print(data_information.text)
         chunks = text_splitter.split_text(data_information.text)
 
-        for chunk, index in chunks:
+        for index, chunk in enumerate(chunks):
             new_data_information = data_information
             new_data_information.text = chunk
-            new_data_information.id = data_information.id + "_" + index
+            new_data_information.id = data_information.id + "_" + str(index)
             new_data_information_list.append(data_information)
 
     return new_data_information_list
