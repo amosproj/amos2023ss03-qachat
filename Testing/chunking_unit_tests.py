80ca2806a2e9bcfe91847e89f0aa17b934fce3bd
Lint Action <lint-action@samuelmeuli.com>
2023-06-12 10:41:14 +0000

    Fix code style issues with Black

--- Testing/chunking_unit_tests.py
+++ Testing/chunking_unit_tests.py
@@ -13,13 +13,14 @@ from typing import List
 
 
 class TestChunking(unittest.TestCase):
-
     def setUp(self):
         self.current_time = datetime.now()
         self.last_updated = datetime(1970, 1, 1)
 
     @staticmethod
-    def __load_preprocessed_data(before: datetime, after: datetime, filepath: str) -> List[DataInformation]:
+    def __load_preprocessed_data(
+        before: datetime, after: datetime, filepath: str
+    ) -> List[DataInformation]:
         df = pd.read_csv(filepath, sep=";")
         raw_data = []
         for index, row in df.iterrows():
@@ -35,17 +36,23 @@ class TestChunking(unittest.TestCase):
         return [data for data in raw_data if after < data.last_changed <= before]
 
     def test_smaller_than_overlap(self):
-        data_list = self.__load_preprocessed_data(self.current_time, self.last_updated, "../DummyData/qa_less_50.csv")
+        data_list = self.__load_preprocessed_data(
+            self.current_time, self.last_updated, "../DummyData/qa_less_50.csv"
+        )
         result = transform_text_to_chunks(data_list)
         self.assertTrue(len(result) is len(data_list))
 
     def test_smaller_than_chunk_size(self):
-        data_list = self.__load_preprocessed_data(self.current_time, self.last_updated, "../DummyData/qa_less_200.csv")
+        data_list = self.__load_preprocessed_data(
+            self.current_time, self.last_updated, "../DummyData/qa_less_200.csv"
+        )
         result = transform_text_to_chunks(data_list)
         self.assertTrue(len(result) is len(data_list))
 
     def test_larger_than_chunk_size(self):
-        data_list = self.__load_preprocessed_data(self.current_time, self.last_updated, "../DummyData/qa_2000.csv")
+        data_list = self.__load_preprocessed_data(
+            self.current_time, self.last_updated, "../DummyData/qa_2000.csv"
+        )
         df = pd.read_csv("../DummyData/qa_2000.csv", sep=";")
         characters = len(df.to_string().strip())
         result = transform_text_to_chunks(data_list)
