65237263edac175b78a8b55af24c49dc7837eeea
Felix-012 <f.nuetzel@gmx.de>
2023-06-19 14:20:47 +0200

    adds reusing of creds so multiple files can be read
    Co-authored-by: Felix-012 <f.nuetzel@gmx.de>
    Co-authored-by: Emanuel Erben <emanuel.erben@gmail.com>
    Co-authored-by: Hafidz Arifin <hafidz.harifin@gmail.com>

--- QAChat/Data_Processing/google_doc_preprocessor.py
+++ QAChat/Data_Processing/google_doc_preprocessor.py
@@ -4,7 +4,6 @@
 # SPDX-FileCopyrightText: 2023 Felix Nützel
 
 from __future__ import print_function
-
 import io
 from pdf_reader import read_pdf
 from googleapiclient.discovery import build
@@ -13,40 +12,47 @@ from googleapiclient.http import MediaIoBaseDownload
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 
-def export_pdf(real_file_id):
-    flow = InstalledAppFlow.from_client_secrets_file(
-        "client_secrets.json", ["https://www.googleapis.com/auth/drive"]
-    )
-    creds = flow.run_local_server()
+class GoogleDocPreProcessor:
 
-    try:
-        # create drive api client
-        service = build("drive", "v3", credentials=creds)
+    def __init__(self):
+        self.creds = None
 
-        file_id = real_file_id
+    def export_pdf(self, real_file_id, creds=None):
+        if creds is None or not creds.valid:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "client_secrets.json", ["https://www.googleapis.com/auth/drive"]
+            )
+            creds = flow.run_local_server()
+            self.creds = creds
 
-        request = service.files().export_media(
-            fileId=file_id, mimeType="application/pdf"
-        )
-        file = io.BytesIO()
-        downloader = MediaIoBaseDownload(file, request)
-        done = False
-        while done is False:
-            status, done = downloader.next_chunk()
-            print(f"Download {int(status.progress() * 100)}.")
+        try:
+            service = build("drive", "v3", credentials=creds)
+
+            request = service.files().export_media(
+                fileId=real_file_id, mimeType="application/pdf"
+            )
+            file = io.BytesIO()
+            downloader = MediaIoBaseDownload(file, request)
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+                print(f"Download {int(status.progress() * 100)}.")
+                print("")
+
+        except HttpError as error:
+            print(f"An error occurred: {error}")
             print("")
+            file = None
 
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        print("")
-        file = None
-
-    return file.getvalue()
+        return file.getvalue()
 
 
 if __name__ == "__main__":
-    print(
-        read_pdf(
-            export_pdf(real_file_id="1UxipR3mJfZjdKslGFZrTLmh78pfjKpfW7HxZ4phuWPs")
-        )
-    )
+    g_doc_proc = GoogleDocPreProcessor()
+    file_data = g_doc_proc.export_pdf(real_file_id="1UxipR3mJfZjdKslGFZrTLmh78pfjKpfW7HxZ4phuWPs",
+                                      creds=g_doc_proc.creds)
+    print(read_pdf(file_data))
+
+    file_data = g_doc_proc.export_pdf(real_file_id="1UxipR3mJfZjdKslGFZrTLmh78pfjKpfW7HxZ4phuWPs",
+                                      creds=g_doc_proc.creds)
+    print(read_pdf(file_data))
